6c94359773075eb2a8b3ad66543a3c73e020dd43
Kirsten Keels <keelsk@gmail.com>
2020-04-15 19:59:30 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -2,4 +2,14 @@ class GenresController < ApplicationController
   def show
     @genre = Genre.find_by_id(params[:id])
   end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to genre_path(@genre)
+  end
 end
