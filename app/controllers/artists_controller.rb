8c9c6650b2c454703c24e3abce7db0f42d395840
Kirsten Keels <keelsk@gmail.com>
2020-04-15 19:49:56 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -6,4 +6,10 @@ class ArtistsController < ApplicationController
   def new
     @artist = Artist.new
   end
+
+  def create
+    @artist = Artist.new(artist_params)
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
 end
