ad0cc9e33abdc6edd0cdbaf72db8fbf9fecbe864
Kirsten Keels <keelsk@gmail.com>
2020-04-15 20:04:20 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,4 +2,19 @@ class SongsController < ApplicationController
   def show
     @song = Song.find_by_id(params[:id])
   end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params)
+    @song.save
+    redirect_to song_path(@song)
+  end
+
+  private
+  def song_params
+    params.require(:song).permit(:name, :artist_id, :genre_id)
+  end
 end
