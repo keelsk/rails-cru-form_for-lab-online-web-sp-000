c2929c6239275becc43b50edcb7ac26aabe2f04e
Kirsten Keels <keelsk@gmail.com>
2020-04-15 19:55:51 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -17,6 +17,12 @@ class ArtistsController < ApplicationController
     @artist = Artist.find_by_id(params[:id])
   end
 
+  def update
+    @artist = Artist.find_by_id(params[:id])
+    @artist.update(artist_params)
+    redirect_to artist_path(@artist)
+  end
+
 
   private
   def artist_params
